a391b042366c1d173cd0a9bbfc5db4f9d1d425a8
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-06 13:10:48 +0100

    + todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import Todo from "./components/Todo";
 
 const App = () => {
-	return <Todo />;
+	const todos = [
+		{ id: 1, title: "wash dishes", completed: false },
+		{ id: 2, title: "make dinner", completed: true }
+	];
+	return (
+		<div>
+			{todos.map(todo => (
+				<Todo todo={todo} key={todo.id} />
+			))}
+		</div>
+	);
 };
 
 export default App;
