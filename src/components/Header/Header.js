4df463646c4ec74bc0b7c05869cd5ebe36439989
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-08 23:55:09 +0100

    -+ Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,9 @@ export default function Header({ title }) {
 			<h1 title="Header" className="header">
 				{title}
 			</h1>
-			{/* <h3 data-testid="header-2" className="header">Hello</h3> */}
+			<h3 data-testid="header-2" className="header">
+				Hello
+			</h3>
 		</>
 	);
 }
